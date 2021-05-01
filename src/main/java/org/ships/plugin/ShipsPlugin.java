cbac94bc430dfc342d781699541352fe8e97cdc3
mosemister <nekronmoseley@gmail.com>
2021-05-01 21:51:44 +0100

    Moved to gradle

--- src/main/java/org/ships/plugin/ShipsPlugin.java
+++ src/main/java/org/ships/plugin/ShipsPlugin.java
@@ -54,10 +54,10 @@ public abstract class ShipsPlugin implements Plugin {
     private final DebugFile debugFile;
     private final Set<Vessel> vessels = new HashSet<>();
 
-    public static final double PRERELEASE_VERSION = 11;
+    public static final double PRERELEASE_VERSION = 11.1;
     public static final String PRERELEASE_TAG = "Beta";
 
-    public ShipsPlugin(){
+    public ShipsPlugin() {
         plugin = this;
         init();
         LegacyShipsConfig legacyShipsConfig = new LegacyShipsConfig();
@@ -66,7 +66,7 @@ public abstract class ShipsPlugin implements Plugin {
         this.blockList = new DefaultBlockList();
         this.debugFile = new DebugFile();
         CorePlugin.getEventManager().register(this, new CoreEventListener());
-        if(this.config.isFallingEnabled()) {
+        if (this.config.isFallingEnabled()) {
             Scheduler fallScheduler = FallExecutor.createScheduler();
             fallScheduler.run();
         }
@@ -76,54 +76,54 @@ public abstract class ShipsPlugin implements Plugin {
 
     @Override
     public void registerCommands(CommandRegister register) {
-        if(this.config.getFile().getBoolean(this.config.ALPHA_COMMAND_USE_LEGACY.getNode()).orElse(true)) {
+        if (this.config.getFile().getBoolean(this.config.ALPHA_COMMAND_USE_LEGACY.getNode()).orElse(true)) {
             register.register(new LegacyShipsCommand());
-        }else{
+        } else {
             register.register(new ShipsArgumentCommand());
         }
     }
 
     public abstract File getShipsConigFolder();
 
-    public MessageConfig getMessageConfig(){
+    public MessageConfig getMessageConfig() {
         return this.messageConfig;
     }
 
-    public void loadCustomShipType(){
+    public void loadCustomShipType() {
         File folder = new File(getShipsConigFolder(), "Configuration/ShipType/Custom");
-        for(CloneableShipType<?> type : getAll(CloneableShipType.class)){
+        for (CloneableShipType<?> type : getAll(CloneableShipType.class)) {
             File folderType = new File(folder, type.getId().replace(":", ".") + "/");
             File[] files = folderType.listFiles();
-            if(files == null){
+            if (files == null) {
                 folderType.mkdirs();
                 continue;
             }
-            for(File file : files){
+            for (File file : files) {
                 this.identifable.add(type.cloneWithName(file));
             }
         }
     }
 
-    public void loadVessels(){
+    public void loadVessels() {
         this.vessels.addAll(ShipsFileLoader.loadAll(Throwable::printStackTrace));
     }
 
-    public void loadConverts(){
+    public void loadConverts() {
         getAll(ShipsConverter.class).forEach(c -> {
             File folder = c.getFolder();
             File[] files = folder.listFiles();
-            if(files == null){
+            if (files == null) {
                 return;
             }
             Stream.of(files).filter(f -> !f.isDirectory()).forEach(f -> {
-                if(c instanceof VesselConverter){
+                if (c instanceof VesselConverter) {
                     try {
-                        registerVessel((Vessel)c.convert(f));
+                        registerVessel((Vessel) c.convert(f));
                     } catch (IOException e) {
                         System.err.println("Error converting vessel with " + c.getId() + " at: " + f.getPath());
                         e.printStackTrace();
                     }
-                }else{
+                } else {
                     try {
                         System.err.println("Error converting file with " + c.getId() + " at: " + f.getPath());
                         register(c.convert(f));
@@ -135,7 +135,7 @@ public abstract class ShipsPlugin implements Plugin {
         });
     }
 
-    private void init(){
+    private void init() {
         this.identifable.add(BasicMovement.SHIPS_FIVE);
         this.identifable.add(BasicMovement.SHIPS_SIX);
         this.identifable.add(BasicBlockFinder.SHIPS_FIVE);
@@ -155,7 +155,7 @@ public abstract class ShipsPlugin implements Plugin {
         this.vesselFlags.put("ships:player_states", new PlayerStatesFlag.Builder());
     }
 
-    private void init2(){
+    private void init2() {
         this.identifable.add(ShipType.OVERPOWERED_SHIP);
         this.identifable.add(ShipType.AIRSHIP);
         this.identifable.add(ShipType.WATERSHIP);
@@ -164,11 +164,11 @@ public abstract class ShipsPlugin implements Plugin {
         this.identifable.add(ShipType.PLANE);
     }
 
-    public ShipsConfig getConfig(){
+    public ShipsConfig getConfig() {
         return this.config;
     }
 
-    public void getLoadedMessages(){
+    public void getLoadedMessages() {
         CorePlugin.getConsole().sendMessage(CorePlugin.buildText(TextColours.RED + "------[Ships Loaded Information][Start]------"));
         displayMessage(BasicBlockFinder.class, "BlockFinders", bf -> "");
         displayMessage(BasicMovement.class, "MovementMethods", bm -> "");
@@ -179,7 +179,7 @@ public abstract class ShipsPlugin implements Plugin {
         CorePlugin.getConsole().sendMessage(CorePlugin.buildText(TextColours.RED + "------[Ships Loaded Information][End]------"));
     }
 
-    private <I extends Identifable> void displayMessage(Class<I> class1, String name, Function<I, String> function){
+    private <I extends Identifable> void displayMessage(Class<I> class1, String name, Function<I, String> function) {
         Set<I> values = getAll(class1);
         CorePlugin.getConsole().sendMessage(CorePlugin.buildText(TextColours.AQUA + "Found " + name + ": " + values.size()));
         values.forEach(v -> {
@@ -190,62 +190,62 @@ public abstract class ShipsPlugin implements Plugin {
         });
     }
 
-    public DebugFile getDebugFile(){
+    public DebugFile getDebugFile() {
         return this.debugFile;
     }
 
-    public DefaultBlockList getBlockList(){
+    public DefaultBlockList getBlockList() {
         return this.blockList;
     }
 
-    public Set<CrewPermission> getDefaultPermissions(){
+    public Set<CrewPermission> getDefaultPermissions() {
         return this.defaultPermissions;
     }
 
-    public Set<Vessel> getVessels(){
+    public Set<Vessel> getVessels() {
         return this.vessels;
     }
 
-    public <T extends Identifable> Set<T> getAll(Class<T> class1){
-        if(ShipsVessel.class.isAssignableFrom(class1)){
-            return (Set<T>)(Set<? extends Vessel>)this.vessels.stream().filter(v -> v instanceof ShipsVessel).collect(Collectors.toSet());
+    public <T extends Identifable> Set<T> getAll(Class<T> class1) {
+        if (ShipsVessel.class.isAssignableFrom(class1)) {
+            return (Set<T>) (Set<? extends Vessel>) this.vessels.stream().filter(v -> v instanceof ShipsVessel).collect(Collectors.toSet());
         }
-        return (Set<T>)identifable.stream().filter(class1::isInstance).collect(Collectors.toSet());
+        return (Set<T>) identifable.stream().filter(class1::isInstance).collect(Collectors.toSet());
     }
 
-    public <T extends Identifable> Optional<T> get(Class<T> class1){
-        return (Optional<T>)identifable.stream().filter(class1::isInstance).findAny();
+    public <T extends Identifable> Optional<T> get(Class<T> class1) {
+        return (Optional<T>) identifable.stream().filter(class1::isInstance).findAny();
     }
 
-    public Map<String, VesselFlag.Builder<?, ?>> getVesselFlags(){
+    public Map<String, VesselFlag.Builder<?, ?>> getVesselFlags() {
         return this.vesselFlags;
     }
 
-    public void registerVessel(@NotNull Vessel vessel){
+    public void registerVessel(@NotNull Vessel vessel) {
         this.vessels.add(vessel);
     }
 
-    public void unregisterVessel(Vessel... vessels){
+    public void unregisterVessel(Vessel... vessels) {
         this.vessels.removeAll(Arrays.asList(vessels));
     }
 
-    public void register(Identifable... identifables){
+    public void register(Identifable... identifables) {
         this.identifable.addAll(Arrays.asList(identifables));
     }
 
-    public void unregister(Identifable... identifables){
+    public void unregister(Identifable... identifables) {
         this.identifable.removeAll(Arrays.asList(identifables));
     }
 
-    public void register(CrewPermission... permissions){
+    public void register(CrewPermission... permissions) {
         this.defaultPermissions.addAll(Arrays.asList(permissions));
     }
 
-    public void register(String id, VesselFlag.Builder<?, ?> flag){
+    public void register(String id, VesselFlag.Builder<?, ?> flag) {
         this.vesselFlags.put(id, flag);
     }
 
-    public static ShipsPlugin getPlugin(){
+    public static ShipsPlugin getPlugin() {
         return plugin;
     }
 
@@ -255,12 +255,12 @@ public abstract class ShipsPlugin implements Plugin {
     }
 
     @Override
-    public String getPluginId(){
+    public String getPluginId() {
         return "ships";
     }
 
     @Override
-    public String getPluginVersion(){
+    public String getPluginVersion() {
         return "6.0.0.0";
     }
 }
