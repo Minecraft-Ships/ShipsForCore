7cc7a59e79b9742e2225b0a31614e15e63395812
mosemister <nekronmoseley@gmail.com>
2021-11-26 21:57:32 +0000

    Updated version tag

--- src/main/java/org/ships/plugin/ShipsPlugin.java
+++ src/main/java/org/ships/plugin/ShipsPlugin.java
@@ -45,7 +45,7 @@ import java.util.stream.Stream;
 
 public class ShipsPlugin implements CorePlugin {
 
-    public static final double PRERELEASE_VERSION = 14.0;
+    public static final double PRERELEASE_VERSION = 14.1;
     public static final String PRERELEASE_TAG = "Beta";
     private static ShipsPlugin plugin;
     private final Map<String, VesselFlag.Builder<?, ?>> vesselFlags = new HashMap<>();
